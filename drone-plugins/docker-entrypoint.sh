c394f6fdf2a5284459617d35b2f1052c361d4b52
Rui Fortes <ruisantosfortes@gmail.com>
2018-07-13 17:55:57 +0100

    bump to 1.2.9

--- drone-plugins/docker-entrypoint.sh
+++ drone-plugins/docker-entrypoint.sh
@@ -3,6 +3,7 @@ set -e
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 export IMAGE_REPO=$(jq -r '.config.imageRepo' < package.json)
 export IMAGE_TAG=$(jq -r '.version' < package.json)
+echo "pushing image ${IMAGE_REPO}:${IMAGE_TAG}"
 docker image build -t $IMAGE_REPO:$IMAGE_TAG .
 docker image tag $IMAGE_REPO:$IMAGE_TAG $IMAGE_REPO:latest
 docker push $IMAGE_REPO:$IMAGE_TAG
