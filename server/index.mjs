f1b882a3f9719ce765e3b034f8cea0c64f63fb30
Rui Fortes <ruisantosfortes@gmail.com>
2018-07-22 16:22:50 +0100

    bump to 1.3.6

--- server/index.mjs
+++ server/index.mjs
@@ -28,7 +28,7 @@ app.use('/', express.static(Path.resolve(DIRNAME, '../client/build')))
 app.use('/api' ,arangoRouter)
 
 app.get('/test', (req, res) => {
-  res.json({version: '1.3.5'})
+  res.json({version: '1.3.6'})
 })
 
 // app.get('/test', (req, res) => {
