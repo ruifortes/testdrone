67bbff688e26616457ff32505ab6a1cffe462996
Rui Fortes <ruisantosfortes@gmail.com>
2018-07-22 21:09:26 +0100

    fix1

--- server/arango.mjs
+++ server/arango.mjs
@@ -4,9 +4,12 @@ const Database = arango.Database
 const aql = arango.aql
 
 const {ARANGODB_HOST, ARANGO_ROOT_PASSWORD} = process.env
+
 const db = new Database({
   url: `http://${ARANGODB_HOST || 'localhost'}:8529`
 })
+
+console.log(`conection to arangodb on http://${ARANGODB_HOST || 'localhost'}:8529 with password=${ARANGO_ROOT_PASSWORD}`);
 
 db.useDatabase('test')
 db.useBasicAuth('root', ARANGO_ROOT_PASSWORD)
